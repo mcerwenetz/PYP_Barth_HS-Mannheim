cbd376cab908bea6cd8c97928cd17ac7e041f831
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-10-16 18:11:24 +0200

    a2 gelöst

--- PyPrgs/Blatt1/a2.py
+++ PyPrgs/Blatt1/a2.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+import sys
+
+def calc(a, op, b):
+    if op == "+":
+        return a+b
+    elif op == "-":
+        return a-b
+    elif op == "*":
+        return a*b
+    elif  op == "/":
+        return a /b
+    else:
+        return "Error"
+
+
+args = sys.argv[1:]
+# print(args)
+# args= args[1:]
+zahl1=float(args[0])
+op = args[1]
+zahl2=float(args[2])
+print(calc(zahl1, op, zahl2))
