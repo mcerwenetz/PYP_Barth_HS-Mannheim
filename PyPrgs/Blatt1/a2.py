36ad0a032610c6f0eb41a1a89e83d300068fc55b
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-10-17 11:25:46 +0200

    Recherechiert wegen destruktiver methode append

--- PyPrgs/Blatt1/a2.py
+++ PyPrgs/Blatt1/a2.py
@@ -4,3 +4,7 @@ liste = [1,2]
 
 #Append mit slicing
 liste[len(liste):] += [3]
+
+#Append ist destruktiv? Wenn ich id(liste) eingebe, dann bekomm ich auch nach dem append die gleiche id wie vorher. 
+#Dann müsste es doch das gleiche Objekt sein?
+#Bei dem slicing-operator kommt allerdings auch die gleiche id raus.
