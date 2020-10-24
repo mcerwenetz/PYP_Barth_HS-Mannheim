f7ebceb5b6621ffd1dd518c89ecf792bf4e371b1
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-10-24 23:16:09 +0200

    heller implementiert

--- PyPrgs/Blatt2/imagemanip.py
+++ PyPrgs/Blatt2/imagemanip.py
@@ -4,13 +4,21 @@ from PIL import Image
 def gamma():
     pass
 
-def heller():
-    pass
+def heller(cols, rows, img, zahl):
+    for y in range(rows):
+        for x in range(cols):
+            v = img.getpixel((x, y))
+            newvalue = int((v + (255.0*zahl)))
+            tupelKoordinate=(x,y)
+            img.putpixel(tupelKoordinate, newvalue)
+    return img
+
 def spreizen():
     pass
+
 def binarisieren(cols, rows, img, zahl):
-    for y in range(rows-1):
-        for x in range(cols-1):
+    for y in range(rows):
+        for x in range(cols):
             v = img.getpixel((x, y))
             if v >= zahl:
                 img.putpixel((x, y), 255)
@@ -20,17 +28,19 @@ def binarisieren(cols, rows, img, zahl):
 
 
 def main(argv):
-    manipulation=argv[0]
-    zahl = float(argv[1])
-    pfad = argv[2]
-    ausgabedatei=argv[3]
+    #manipulation=argv[0]
+    #zahl = float(argv[1])
+    zahl=0.50
+    #pfad = argv[2]
+    #ausgabedatei=argv[3]
 
-    img =Image.open(r'D:\Nextcloud\Studium\6TIB\PYP\Repo\20pyth06\PyPrgs\Blatt2\bilder\a.pgm')
+    img =Image.open(r'C:\Users\mariu\Nextcloud\Studium\6TIB\PYP\Repo\20pyth06\PyPrgs\Blatt2\a.pgm')
     cols, rows = img.size
+  #  img.show()
+    img = heller(cols,rows,img,zahl)
+    #img = binarisieren(cols,rows,img,zahl)
     img.show()
-    img = binarisieren(cols,rows,img,zahl)
-    img.show()
-    img.save(ausgabedatei)
+    #img.save(ausgabedatei)
 
 
 if __name__ == "__main__":
