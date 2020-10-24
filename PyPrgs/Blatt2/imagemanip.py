12a42bd850949b0bea54bb0c4da697082539cf53
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-10-24 23:26:46 +0200

    gamma implementiert

--- PyPrgs/Blatt2/imagemanip.py
+++ PyPrgs/Blatt2/imagemanip.py
@@ -1,8 +1,14 @@
 import sys
 from PIL import Image
 
-def gamma():
-    pass
+def gamma(cols,rows,img, zahl):
+    for y in range(rows):
+        for x in range(cols):
+            v = img.getpixel((x, y))
+            newvalue = int(255*pow((v/255),zahl))
+            tupelKoordinate=(x,y)
+            img.putpixel(tupelKoordinate, newvalue)
+    return img
 
 def heller(cols, rows, img, zahl):
     for y in range(rows):
@@ -30,14 +36,15 @@ def binarisieren(cols, rows, img, zahl):
 def main(argv):
     #manipulation=argv[0]
     #zahl = float(argv[1])
-    zahl=0.50
+    zahl=5
     #pfad = argv[2]
     #ausgabedatei=argv[3]
 
     img =Image.open(r'C:\Users\mariu\Nextcloud\Studium\6TIB\PYP\Repo\20pyth06\PyPrgs\Blatt2\a.pgm')
     cols, rows = img.size
-  #  img.show()
-    img = heller(cols,rows,img,zahl)
+    # img.show()
+    # img = heller(cols,rows,img,zahl)
+    img = gamma(cols,rows,img,zahl)
     #img = binarisieren(cols,rows,img,zahl)
     img.show()
     #img.save(ausgabedatei)
