8dc6fc4877e9a10fdbdfd6c8328076b12240355d
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-11 19:30:52 +0100

    vergleich jetzt mit Liste

--- PyPrgs/Blatt4/a2.py
+++ PyPrgs/Blatt4/a2.py
@@ -11,7 +11,12 @@ dic = {}
 dic = { (x,y,a,b):sum((pow(x,3),pow(y,3))) 
 for x in range(1,g+1) for y in range(1,g+1) for a in range(1,g+1) for b in range(1,g+1) 
 if sum((pow(x,3),pow(y,3))) == sum((pow(a,3),pow(b,3))) 
-and x != y and x != a and x != b and y != a and y !=b and b != a and (x,y,a,b) }
+# and x != y and x != a and x != b and y != a and y !=b and b != a}
+and x not in [y,a,b]
+and y not in [x,a,b]
+and a not in [x,y,b]
+and b not in [x,y,a]
+}
 
 print(dic)
 
