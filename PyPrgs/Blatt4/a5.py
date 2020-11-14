7d1ffbe880551a7cc8be9312a37ae8211a41f47f
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-14 13:45:49 +0100

    dictionary auf stringvalues gemapped

--- PyPrgs/Blatt4/a5.py
+++ PyPrgs/Blatt4/a5.py
@@ -1,17 +1,18 @@
 dictionary = {x:1 for x in "bfpv"}
-dictionary.update({x:2 for x in "cgjkqsxz"})
-dictionary.update({x:3 for x in "dt"})
-dictionary.update({x:4 for x in "l"})
-dictionary.update({x:5 for x in "mn"})
-dictionary.update({x:6 for x in "r"})
+dictionary.update({x:'2' for x in "cgjkqsxz"})
+dictionary.update({x:'3' for x in "dt"})
+dictionary.update({x:'4' for x in "l"})
+dictionary.update({x:'5' for x in "mn"})
+dictionary.update({x:'6' for x in "r"})
 dictionary.update({x:"" for x in "aeiouwyh"})
 
 
 def soundex(zeichenkette : str) -> str:
+    zeichenkette = zeichenkette.lower()
     output = zeichenkette[0]
     for letter in zeichenkette[1:]:
         if dictionary[letter] != output[-1]:
-            output = output + str(dictionary[letter])
+            output = output + dictionary[letter]
         if len(output) >= 6:
             break
     while len(output) <6:
@@ -29,4 +30,4 @@ def main(inputs):
     print(ergebnisse)
 
 if __name__ == "__main__":
-    main(["soundex", "soundeggs", "flurbel"])
+    main(["hallo"])
