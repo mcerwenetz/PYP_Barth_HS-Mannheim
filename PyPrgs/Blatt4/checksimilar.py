09cf5efbedc2e2c37b42aed9fd39d918369268a9
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-14 14:37:39 +0100

    checksimilar implementiert

---
+++ PyPrgs/Blatt4/checksimilar.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+from a5 import soundex
+import string
+
+def main():
+    file = open("a.txt")
+    filelines = file.readlines();
+    dic = {}
+
+    for line in filelines:
+        line = line.rstrip("\n")
+        fileWordAsList = [char for char in line]
+        sAscii = [char for char in fileWordAsList if char in string.ascii_letters]
+        sAsciistr = ''
+        for c in sAscii:
+            sAsciistr+=c
+        lineAsSoundex = soundex(line)
+        
+        if not lineAsSoundex in dic:
+            dic[lineAsSoundex] = [line]
+        else:
+            dic[lineAsSoundex] = dic[lineAsSoundex] + [line]
+
+    print(dic)
+    # values = list(dic.values())
+    
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
