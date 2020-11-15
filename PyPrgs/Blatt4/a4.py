50a3610e82b524517849352c87bdcec9530e19f5
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-15 14:05:58 +0100

    rot13 funktioniert

--- PyPrgs/Blatt4/a4.py
+++ PyPrgs/Blatt4/a4.py
@@ -1,9 +1,21 @@
-
+from string import ascii_lowercase
 translationTable = {}
+
+for i in range(0,26):
+    if i < 13:
+        translationTable[ascii_lowercase[i]] = ascii_lowercase[i+13]
+    else:
+        translationTable[ascii_lowercase[i]] = ascii_lowercase[(i+13)%len(ascii_lowercase)]
 
 
 def rot13(decoded: str) -> str:
     coded = ""
 
+    for letter in decoded:
+        coded += translationTable[letter]
+
     return coded
-    pass
+
+
+print(rot13(rot13("hallo")))
+
