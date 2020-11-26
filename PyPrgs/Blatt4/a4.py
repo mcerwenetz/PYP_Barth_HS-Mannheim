db42331f7c27719038393cec8b2e3b953b3f1d40
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-26 16:08:50 +0100

    a4: added comments

--- PyPrgs/Blatt4/a4.py
+++ PyPrgs/Blatt4/a4.py
@@ -1,12 +1,14 @@
 from string import ascii_lowercase
 translationTable = {}
+ascii_lowercaseAsList = [char for char in ascii_lowercase]
 
-for i in range(0,26):
-    if i < 13:
-        translationTable[ascii_lowercase[i]] = ascii_lowercase[i+13]
-    else:
-        # janz wichtig! modulo damit er wieder von vorne anfängt
-        translationTable[ascii_lowercase[i]] = ascii_lowercase[(i+13)%len(ascii_lowercase)]
+# for i in range(0,26):
+#     if i < 13:
+#         translationTable[ascii_lowercase[i]] = ascii_lowercase[i+13]
+#     else:
+#         # janz wichtig! modulo damit er wieder von vorne anfängt
+#         translationTable[ascii_lowercase[i]] = ascii_lowercase[(i+13)%len(ascii_lowercase)]
+translationTable.update({ascii_lowercaseAsListChar:chr(ord(ascii_lowercaseAsListChar)+13) if ord(ascii_lowercaseAsListChar)+13 <= ord('z') else chr((ord(ascii_lowercaseAsListChar)+13)-26) for ascii_lowercaseAsListChar in ascii_lowercaseAsList  })
 
 
 def rot13(decoded: str) -> str:
