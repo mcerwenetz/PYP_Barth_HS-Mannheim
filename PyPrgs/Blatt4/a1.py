974d7e8bc3a0a0bf3ab9bd83042dfd705e1ef107
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-11-26 16:09:28 +0100

    a1: primitive lambdas auskommentiert

--- PyPrgs/Blatt4/a1.py
+++ PyPrgs/Blatt4/a1.py
@@ -2,7 +2,7 @@
 print([i**3 for i in range(1,11) if i%2 == 0])
 
 # oder als lambda
-print(list(filter(lambda x: x %2 == 0, map(lambda x: x**3, range(1,11)))))
+# print(list(filter(lambda x: x %2 == 0, map(lambda x: x**3, range(1,11)))))
 
 
 # Alle Teiler einer Zahl z außer 1 und z (testen Sie mit 123, 12345, 123456)
@@ -10,7 +10,7 @@ zahl = 123
 print([i for i in range(2,zahl) if zahl%i == 0])
 
 # primitiv
-print(list(filter(lambda i: zahl%i == 0, range(2,zahl))))
+# print(list(filter(lambda i: zahl%i == 0, range(2,zahl))))
 
 
 # Alle Primzahlen zwischen 10000 und 10100
@@ -21,4 +21,4 @@ def isPrime(x: int):
     return True
 
 print([i for i in range(10000, 10100) if isPrime(i)])
-print(list(filter(isPrime, range(10000, 10100))))
+# print(list(filter(isPrime, range(10000, 10100))))
