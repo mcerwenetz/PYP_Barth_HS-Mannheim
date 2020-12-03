a55db803f2c64e03d58ad514d779d0114b14ccbc
Marius Cerwenetz <cerwenetz.marius@gmail.com>
2020-12-03 19:53:00 +0100

    geht jetzt alle files durch und sucht sich alle zitate raus leider funktionier -m noch nicht
    problem: ignoriert den mitgegebenen string.
    vielleicht an kleinererdateimenge prüfen

--- PyPrgs/Blatt3/fortune.py
+++ PyPrgs/Blatt3/fortune.py
@@ -1,16 +1,24 @@
-import sys, os, string, random
+import sys, os, string, random, io
 
 def main(arguments):
     # Todo: Alle Dateien öffnen wo kein dat oder u8 hintendran ist
     # wenn mehrere wörter gefunden werden sollen list of args entgegennehmen
     # Alle lines aller dateien einlesen trennung bei \n%\n
 
-    # listOfFiles = []
-    # for(dirpath, dirnames, filenames) in  os.walk(r"fortunes"):
-    #     listOfFiles += [os.path.join(dirpath, file) for file in filenames]
-    with open("beispielfortune.txt") as datei:
-        lines = datei.read()
-    listOfAllFortunes = lines.split("\n%\n")
+    listOfFiles = []
+    listOfAllowedFiles=[]
+    listOfAllFortunes=[]
+    for(dirpath, _, filenames) in  os.walk("fortunes"):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+    relevantFiles=filter(lambda x: not x.endswith(".dat"), listOfFiles)
+    for relevantFile in relevantFiles:
+        listOfAllowedFiles.append(relevantFile)
+
+    for allowedFile in listOfAllowedFiles:
+        with open(allowedFile,"r", encoding="utf-8") as datei:
+            lines = datei.read()
+        listOfAllFortunes += lines.split("\n%\n")
     if len(arguments) != 0 and arguments[0] == "-m":
         listOfAllowedFortunes=[]
         search = arguments[1]
