4dc08f5b98249d299f96e2ebf55b24e8b7f09045
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-12-03 13:12:51 +0100

    augaben hinzugefügt

--- PyPrgs/Blatt3/fortune.py
+++ PyPrgs/Blatt3/fortune.py
@@ -2,6 +2,13 @@ import sys, os, string
 
 def main():
     # Todo: Alle Dateien öffnen wo kein dat oder u8 hintendran ist
+    # Alle lines aller dateien einlesen trennung bei \n%\n
+    # random eins ausgeben
+    # teil 2:
+    # auch all das machen aber checken ob -m "wörter" in den args stehen
+    # liste erstellen für alle zitate in der fetten liste die das wort enthalten
+    # liste aussortieren, wenn's vorkommt in die liste für das wort
+    # liste ausgeben  
 
 
 
