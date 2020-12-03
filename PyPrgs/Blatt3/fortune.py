2042950d806b3ffaaa370e3237dfc75dddb64432
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2020-12-03 13:46:34 +0100

    beispiel funktioniert schonmal
    problem war tatsächlich der path.
    scheiß ide.

--- PyPrgs/Blatt3/fortune.py
+++ PyPrgs/Blatt3/fortune.py
@@ -1,9 +1,8 @@
-import sys, os, string
+import sys, os, string, random
 
 def main():
     # Todo: Alle Dateien öffnen wo kein dat oder u8 hintendran ist
     # Alle lines aller dateien einlesen trennung bei \n%\n
-    # random eins ausgeben
     # teil 2:
     # auch all das machen aber checken ob -m "wörter" in den args stehen
     # liste erstellen für alle zitate in der fetten liste die das wort enthalten
@@ -15,7 +14,10 @@ def main():
     #     listOfFiles += [os.path.join(dirpath, file) for file in filenames]
     listOfAllFortunes=[]
     with open("beispielfortune.txt") as datei:
-        lines = datei.readlines()
+        lines = datei.read()
+        listOfAllFortunes = lines.split("\n%\n")
+        randomzitat = random.choice(listOfAllFortunes)
+        print(randomzitat)
 
 
 
